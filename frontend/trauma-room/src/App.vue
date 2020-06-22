d015dcf3f9112421fa721b734a2b025ff9a58786
KieferJan <jkiefer95@gmail.com>
2020-06-22 20:46:02 +0200

    setup components

--- frontend/trauma-room/src/App.vue
+++ frontend/trauma-room/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import header from "./components/header.vue";
+import mainComponent from "./components/mainComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    header,
+    mainComponent
   }
 };
 </script>
---
+++ frontend/trauma-room/src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>EMC-Portal</b></b-nav-item>
+      <b-nav-item>RTW wechseln</b-nav-item>
+      <b-nav-item disabled>Universitätsklinikum Mannheim</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
