360967ae6793dbbad868dc81bcab0c2d27fcee02
KieferJan <jkiefer95@gmail.com>
2020-06-25 11:13:49 +0200

    add basic navigation and map component

--- frontend/trauma-room/src/App.vue
+++ frontend/trauma-room/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    <Header/>
-    <PatientData/>
-    <RightSidebar/>
+    <Header />
+    <PatientData />
+    <b-container fluid align-v="stretch">
+      <b-row>
+        <b-col cols="2"><LeftSidebar /></b-col>
+        <b-col cols="8"><MainComponent /></b-col>
+        <b-col cols="2"><RightSidebar /></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import Header from "./components/header.vue";
 import PatientData from "./components/patientData.vue";
-import RightSidebar from "./components/rightSidebar";
+import RightSidebar from "./components/rightSidebar.vue";
+import LeftSidebar from "./components/leftSidebar.vue";
+import MainComponent from "./components/mainComponent.vue";
 
 //import MainComponent from "./components/mainComponent.vue";
 
@@ -18,7 +26,10 @@ export default {
   components: {
     Header,
     PatientData,
-    RightSidebar
+    RightSidebar,
+    LeftSidebar,
+    MainComponent,
+
   }
 };
 </script>
@@ -30,6 +41,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
 }
 </style>
---
+++ frontend/trauma-room/src/components/leftSidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-button-group vertical>
+      <b-button @click="setSelection($event)" id="btn-puls"
+        >Puls Oxy <br />
+        Puls: 192 <br />
+        SpO2: 98</b-button
+      >
+      <b-button @click="setSelection($event)" id="btn-stream"
+        >Live Bild</b-button
+      >
+      <b-button @click="setSelection($event)" id="btn-position"
+        >Position <br />
+        ETA: 5 min</b-button
+      >
+    </b-button-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showComponent: false,
+      pastEvent: null,
+      selection: []
+    };
+  },
+  methods: {
+    setSelection(event) {
+      if (!this.showComponent || event.currentTarget.id !== this.pastEvent) {
+        //this.selection.push(event.currentTarget.id);
+        this.$root.$emit("selectedComponent", event.currentTarget.id);
+        this.showComponent = true;
+        this.pastEvent = event.currentTarget.id;
+      } else if (event.currentTarget.id == this.pastEvent) {
+        this.$root.$emit("selectedComponent", null);
+        this.showComponent = false;
+        this.pastEvent = event.currentTarget.id;
+      }
+    }
+  }
+};
+</script>
