b7a17f72cbcffbccf626c71b2860305c8632658a
KieferJan <jkiefer95@gmail.com>
2020-06-26 08:08:08 +0200

    Revert "add basic navigation and map component"
    This reverts commit 360967ae6793dbbad868dc81bcab0c2d27fcee02.

--- frontend/trauma-room/src/components/pulsOxy.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    PulsOxy
-  </div>
-</template>
--- frontend/trauma-room/src/main.js
+++ frontend/trauma-room/src/main.js
@@ -3,7 +3,6 @@ import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'leaflet/dist/leaflet.css';
 
 =======
 >>>>>>> parent of d015dcf... setup components
