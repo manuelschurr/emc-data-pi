c37a8e915b7bfee25b984c6bb66200d198a1abca
KieferJan <jkiefer95@gmail.com>
2020-06-26 08:52:14 +0200

    undo changes

--- frontend/trauma-room/src/main.js
+++ frontend/trauma-room/src/main.js
@@ -1,15 +1,4 @@
 import Vue from "vue";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'leaflet/dist/leaflet.css';
-
-=======
->>>>>>> parent of d015dcf... setup components
-=======
->>>>>>> parent of d015dcf... setup components
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
