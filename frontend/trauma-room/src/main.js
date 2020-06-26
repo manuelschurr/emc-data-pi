ad83d8f64722c4baa75a2c821285d23bc610be3f
KieferJan <jkiefer95@gmail.com>
2020-06-26 08:25:06 +0200

    Revert "setup components"
    This reverts commit d015dcf3f9112421fa721b734a2b025ff9a58786.

--- frontend/trauma-room/src/main.js
+++ frontend/trauma-room/src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'leaflet/dist/leaflet.css';
 
+=======
+>>>>>>> parent of d015dcf... setup components
 =======
 >>>>>>> parent of d015dcf... setup components
 import App from "./App.vue";
