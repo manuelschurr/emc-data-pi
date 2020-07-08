8bf6a1f61d72b41c0156880e38c7c9b95458ae1b
Tim <>
2020-07-08 11:41:03 +0200

    Update pulox.py

--- backend/src/pulox.py
+++ backend/src/pulox.py
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-import serial, subprocess
+import serial, subprocess, time
 from datetime import datetime
 
 #set device connection port and save file
@@ -11,55 +11,61 @@ outfile = '/home/pi/Documents/PulsOxy_data.csv'
 bashCommand = 'sudo chmod 777 ' + device
 subprocess.check_call(bashCommand.split())
 
-#setup connection via serial port
+#establish connection via serial port
 ser = serial.Serial()
-def establish_serial_connection(ser):
-    ser.baudrate = 115200          
-    ser.bytesize = serial.EIGHTBITS    
-    ser.parity = serial.PARITY_NONE     
-    ser.stopbits = serial.STOPBITS_ONE  
-    ser.xonxoff = 1                   
-    ser.timeout = 1
-    ser.port = device
+ser.baudrate = 115200          
+ser.bytesize = serial.EIGHTBITS    
+ser.parity = serial.PARITY_NONE     
+ser.stopbits = serial.STOPBITS_ONE  
+ser.xonxoff = 1                   
+ser.timeout = 1
+ser.port = device
+ser.open()
+ser.write(b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80')
 
-#get data from device
-def get_data(ser):
-    ser.open()
-    ser.write(b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80')
+#check if data input stream is received
+try:
+    raw = ser.read(9)
+except:
+    print ('No data stream found.')
+    exit()
+    
+#wait for data recording until pulsoximeter is applied to patient for the first time
+fingerIn = raw[5] & 0x7f
 
+while(fingerIn == 0):
     try:
-        raw = list(ser.read(9))
+        raw = ser.read(9)
     except:
         print ('Data read error.')
         exit()
 
-    with open(outfile, 'a') as csvWrite:
-        csvWrite.write('Timestamp,' + 'Puls Rate,' + 'SpO2\n')
+    fingerIn = raw[5] & 0x7f
 
-        while len(raw) >= 9:
-            now = datetime.now()
-            time = now.strftime('%H:%M:%S.%f') [:-3]
-            pulsRate = raw[5] & 0x7f
-            spo2 = raw[6] & 0x7f
+#testing only
+with open(outfile, 'a') as csvWrite:
+    csvWrite.write('Timestamp,' + 'Puls Rate,' + 'SpO2\n')
 
-            csvWrite.write(str(time) + ', ' + str(pulsRate) + ', ' + str(spo2) + '\n')
+    #obtain data from component
+    while len(raw) >= 9:
+        time = datetime.now().strftime('%H:%M:%S.%f') [:-3]
+        pulsRate_i = raw[5] & 0x7f
+        spo2_i = raw[6] & 0x7f
+        pulsRate = [time, pulsRate_i]
+        sp2o = [time, spo2_i]
 
-            #testing only
-            print(time, str(pulsRate), str(spo2))
-            
-            try:
-                raw = ser.read(9)
-            except:
-                print ('Data read error.')
-                exit()
+        #testing only
+        csvWrite.write(str(time) + ', ' + str(pulsRate_i) + ', ' + str(spo2_i) + '\n')
+        print(time, str(pulsRate_i), str(spo2_i))
 
-        ser.close()
-
-        if len(raw) <= 1:
+        try:
+            raw = ser.read(9)
+        except:
             print ('Data read error.')
             exit()
-        return raw
 
-#execute functions
-establish_serial_connection(ser)
-data = get_data(ser)
+    if len(raw) <= 1:
+        print ('Data read error.')
+        exit()
+
+    ser.close()
