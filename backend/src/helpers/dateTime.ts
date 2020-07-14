ddecd93c8de14a53913381f8a0ebd53426372a0d
Nico Bollmann <nico.bollmann94@gmail.com>
2020-07-14 15:36:16 +0200

    Added GNSS functionality

---
+++ backend/src/helpers/dateTime.ts
@@ -0,0 +1,16 @@
+function IntTwoChars(i: number) {
+   return (`0${i}`).slice(-2);
+}
+
+function getCurrentDateAsTimeStamp() {
+   let date = new Date();
+   let day = IntTwoChars(date.getDate());
+   let month = IntTwoChars(date.getMonth() + 1);
+   let year = date.getFullYear();
+   let hours = IntTwoChars(date.getHours());
+   let minutes = IntTwoChars(date.getMinutes());
+   let seconds = IntTwoChars(date.getSeconds());
+   let dateTimestamp = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+
+   return dateTimestamp;
+}
