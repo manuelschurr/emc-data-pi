8efe306b5bb496cd3014be2a785adfd247e2a851
Manuel Schurr <admin@manuelschurr.com>
2020-08-05 21:26:24 +0100

    Formatting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,25 @@
 <template>
-    <div id="app">
-        <UI msg="Patienten Stammdaten" />
-    </div>
+  <div id="app">
+    <UI msg="Patienten Stammdaten" />
+  </div>
 </template>
 
 <script>
 import UI from "./components/UI.vue";
 export default {
-    name: "App",
-    components: {
-        UI,
-    },
+  name: "App",
+  components: {
+    UI,
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
