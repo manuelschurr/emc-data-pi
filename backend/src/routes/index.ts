bc32025454937f268179b08e00f09a7dc30887da
Nico Bollmann <nico.bollmann94@gmail.com>
2020-08-10 14:18:50 +0200

    Redesigned database connection. Rewritten patient routes.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,10 @@
 import express from "express"
-import audio from "./audio/audio"
+//import audio from "./audio/audio"
 import patient from "./patient/patient"
 
 const router = express.Router()
 
 router.use("/patient", patient)
-router.use("/audio", audio)
+//router.use("/audio", audio)
 
 export default router
