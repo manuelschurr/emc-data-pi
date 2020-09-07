072744363171a40939193096428f7a916e788bb1
Martin Wendnagel <martinwendnagel@gmail.com>
2020-09-07 15:22:47 +0200

    Added Screenshot Dialog

--- frontend/src/components/Camera.vue
+++ frontend/src/components/Camera.vue
@@ -7,7 +7,8 @@
         <!-- Video -->
         <div class="videoAndButton">
             <div class="stream">
-                <video ref="video" width="320" height="180" controls poster="../assets/videoIcon2.png">
+                <!-- <video ref="video" width="448" height="252" controls poster="../assets/videoIcon2.png"> -->
+                <video ref="video" width="448" height="252" poster="../assets/videoIcon2.png">
                     <source="" type="video/mp4" />
                     <p>Ihr Browser verhindert das Abspielen von Video Tags. Sie sollten einen anderen Browser verwenden.</p>
                 </video>
@@ -15,12 +16,15 @@
             <!-- Start and Stop Stream Buttons -->
             <!-- Video Stream Start - Button -->
             <div class="streamButtons">
-                <button type="button" id="button_play" class="btn btn-dark btn-lg" v-on:click="capture()" style="margin-right: 0.5vw">
+                <button type="button" id="button_play" class="btn btn-dark btn-lg" v-on:click="capture()" style>
                     <i class="fa fa-camera"></i>
                 </button>
+                <div class="screenshotMsg" style="position: absolute; right: 10px; top: 30px;">
+                    <div id="screenshotMessage"></div>
+                </div>
                 <!-- Video Stream Pause - Button -->
                 <!-- <button type="button" id="button_stop" class="btn btn-danger btn-lg" onclick="buttonStopPress()">
-                <i class="fa fa-stop"></i>
+                    <i class="fa fa-stop"></i>
                 </button>-->
             </div>
             <Modals />
@@ -69,16 +73,25 @@ export default {
          * and to store it in backend to further process it to trauma-room
          */
         capture() {
+            // Pausing video until it has been processed for user to see the actual screenshot
+            // Accessing video and taking screenshot of it and then POST it to backend
             this.video = this.$refs.video;
-            // document.getElementById("screenshotModal");
-            // var screenshotHeader = document.createElement("div");
-            // screenshotHeader.setAttribute("class", "modal-header");
-            // screenshotModal.appendChild(screenshotHeader);
-            // var video = document.getElementById("video");
+            this.video.pause();
+            // Dialogue to show when screenshot is being sent (should also show the sent screenshot somehow)
+            var screenshotMessage = document.getElementById(
+                "screenshotMessage"
+            );
+            // Test
+            var screenshotSpinner = document.createElement("div");
+            screenshotSpinner.setAttribute(
+                "class",
+                "d-flex align-items-center"
+            );
+            screenshotSpinner.setAttribute("style", "float:left; margin:10px");
+            screenshotSpinner.innerHTML =
+                '<div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>';
+            screenshotMessage.appendChild(screenshotSpinner);
             this.canvas = document.createElement("canvas");
-            // canvas.width = video.videoWidth;
-            // canvas.height = video.videoHeight;
-            // draw the video at that frame
             var context = this.canvas.getContext("2d");
             context.drawImage(this.video, 0, 0, 640, 480);
             // convert it to a usable data URL
@@ -97,12 +110,29 @@ export default {
             })
                 .then((response) => {
                     console.log(response);
-                    // show success message
-                    vm.$modal.show("screenshotModal");
+                    // // show success message
+                    // vm.$modal.show("screenshotModal");
+                    // display success message
+                    screenshotSpinner.innerHTML =
+                        "Screenshot <br> erfolgreich <br> versendet.";
+                    // start video again after some time where screenshot can be observed
+                    setTimeout(() => {
+                        // still holding video to see screenshot and after 5 seconds playing again
+                        vm.video.play();
+                        // remove screenshotmessage again then
+                        screenshotSpinner.innerHTML = "";
+                    }, 5000);
                 })
                 .catch(function (error) {
                     console.log(error);
                     vm.$modal.show("errorModal");
+                    // Unten auskommentieren nachher, Modal reicht hier!
+                    // screenshotSpinner.innerHTML =
+                    //     "Screenshot <br> nicht <br> versendet.";
+                    // hide Spinner again
+                    // No screenshot taken, play video again
+                    screenshotSpinner.innerHTML = "";
+                    vm.video.play();
                 });
 
             // this.canvas = this.$refs.canvas;
@@ -142,6 +172,7 @@ export default {
 .stream {
     display: inline-block;
 }
+
 /*
 .camera {
   width: 100vw;
