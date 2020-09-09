206b47c7e1ff241faae737b9d9da9df9a4c3ee63
Manuel Schurr <admin@manuelschurr.com>
2020-09-09 10:41:49 +0200

    Fixed audio recording - webm extension instead of mp3

--- frontend/src/components/Recording.vue
+++ frontend/src/components/Recording.vue
@@ -1,23 +1,23 @@
 <template>
-    <div>
-        <!-- Voice Record - Row + Button -->
-        <div class="form-group row">
-            <label for="Audio" class="col-2 col-form-label labelTop">Audio</label>
-            <VueRecordAudio mode="press" @result="onResult" />
-            <!-- Container, um die Recordings anzuzeigen -->
-            <div id="audio" class="audio" controls></div>
-            <!-- Bestätigungsnachricht/Errornachricht -->
-            <div id="audioMessage"></div>
-            <!-- <div v-if="audioMsg">Audio erfolgreich an Schockraum gesendet!</div> -->
-            <!-- <div v-if="!audioMsg">Audio nicht erfolgreich an Schockraum gesendet!</div> -->
-            <!-- Recording loeschen Moeglichkeit -->
-            <!-- <div v-for="(record, index) in recordings" :key="index" class="recorded-item">
+  <div>
+    <!-- Voice Record - Row + Button -->
+    <div class="form-group row">
+      <label for="Audio" class="col-2 col-form-label labelTop">Audio</label>
+      <VueRecordAudio mode="press" @result="onResult" />
+      <!-- Container, um die Recordings anzuzeigen -->
+      <div id="audio" class="audio" controls></div>
+      <!-- Bestätigungsnachricht/Errornachricht -->
+      <div id="audioMessage"></div>
+      <!-- <div v-if="audioMsg">Audio erfolgreich an Schockraum gesendet!</div> -->
+      <!-- <div v-if="!audioMsg">Audio nicht erfolgreich an Schockraum gesendet!</div> -->
+      <!-- Recording loeschen Moeglichkeit -->
+      <!-- <div v-for="(record, index) in recordings" :key="index" class="recorded-item">
                 <button @click="removeRecord(index)" class="button btn-dark">
                     <i class="fa fa-trash"></i>
                 </button>
-            </div>-->
-        </div>
+      </div>-->
     </div>
+  </div>
 </template>
 
 <script>
@@ -30,83 +30,83 @@ import axios from "axios";
 
 Vue.use(VueRecord);
 export default {
-    data() {
-        return {
-            audio: [],
-            // audioMsg: false,
-        };
-    },
-    methods: {
-        onResult(data) {
-            var audio = document.getElementById("audio");
-            var mainaudio = document.createElement("audio");
-            var vm = this;
-            // if audio array is empty, display this recording and fill array with it
-            if (this.audio.length === 0) {
-                mainaudio.setAttribute("controls", "controls");
-                audio.appendChild(mainaudio);
-                var audioFile = URL.createObjectURL(data);
-                mainaudio.innerHTML =
-                    '<source src="' + audioFile + '" type="audio/webm" />';
-                this.audio.push(audioFile);
-                console.log(mainaudio);
-            }
-            // if audio array is not empty, clear it and fill array with current recording and display it
-            else {
-                // empty array
-                vm.audio = [];
-                // empty mainaudio.innerHTML
-                audio.innerHTML = "";
-                console.log(mainaudio);
-                // repeat above steps
-                mainaudio.setAttribute("controls", "controls");
-                audio.appendChild(mainaudio);
-                audioFile = URL.createObjectURL(data);
-                mainaudio.innerHTML =
-                    '<source src="' + audioFile + '" type="audio/webm" />';
-                this.audio.push(audioFile);
-            }
-            //this.audio.push(data);
-            //this.audio.push(audioFile);
-            /**
-             * sending audio to PI
-             */
-            const formData = new FormData();
-            const time = moment().format("YYYY-MM-DD_HH-mm-ss");
-            formData.append("audio", data, `${time}.mp3`);
-            // falsch: formData.append("audio", audioFile, `${time}.mp3`)
-            // var vm = this;
-            var audioMessage = document.getElementById("audioMessage");
-            // Test
-            var audioSpinner = document.createElement("div");
-            audioSpinner.setAttribute("class", "d-flex align-items-center");
-            audioSpinner.innerHTML =
-                '<strong>Sende Audio...</strong><div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>';
-            audioMessage.appendChild(audioSpinner);
-            console.log("Sending the blob data:", data);
-            console.log("Downloadable audio", window.URL.createObjectURL(data));
-            axios({
-                method: "post",
-                url: "https://localhost:3000/audio",
-                headers: {
-                    // "Content-Type": "audio/webm",
-                    "Content-Type": "multipart/form-data",
-                },
-                data: formData,
-            })
-                .then(function (response) {
-                    console.log(response);
-                    // vm.audioMsg = true;
-                    audioMessage.innerHTML =
-                        '<strong><p style="color:green">Audio versendet!</p></strong>';
-                })
-                .catch(function (error) {
-                    console.log(error);
-                    // vm.audioMsg = false;
-                    audioMessage.innerHTML =
-                        '<strong><p style="color:red">Audio nicht versendet!</p></strong>';
-                });
+  data() {
+    return {
+      audio: [],
+      // audioMsg: false,
+    };
+  },
+  methods: {
+    onResult(data) {
+      var audio = document.getElementById("audio");
+      var mainaudio = document.createElement("audio");
+      var vm = this;
+      // if audio array is empty, display this recording and fill array with it
+      if (this.audio.length === 0) {
+        mainaudio.setAttribute("controls", "controls");
+        audio.appendChild(mainaudio);
+        var audioFile = URL.createObjectURL(data);
+        mainaudio.innerHTML =
+          '<source src="' + audioFile + '" type="audio/webm" />';
+        this.audio.push(audioFile);
+        console.log(mainaudio);
+      }
+      // if audio array is not empty, clear it and fill array with current recording and display it
+      else {
+        // empty array
+        vm.audio = [];
+        // empty mainaudio.innerHTML
+        audio.innerHTML = "";
+        console.log(mainaudio);
+        // repeat above steps
+        mainaudio.setAttribute("controls", "controls");
+        audio.appendChild(mainaudio);
+        audioFile = URL.createObjectURL(data);
+        mainaudio.innerHTML =
+          '<source src="' + audioFile + '" type="audio/webm" />';
+        this.audio.push(audioFile);
+      }
+      //this.audio.push(data);
+      //this.audio.push(audioFile);
+      /**
+       * sending audio to PI
+       */
+      const formData = new FormData();
+      const time = moment().format("YYYY-MM-DD_HH-mm-ss");
+      formData.append("audio", data, `${time}.webm`);
+      // falsch: formData.append("audio", audioFile, `${time}.mp3`)
+      // var vm = this;
+      var audioMessage = document.getElementById("audioMessage");
+      // Test
+      var audioSpinner = document.createElement("div");
+      audioSpinner.setAttribute("class", "d-flex align-items-center");
+      audioSpinner.innerHTML =
+        '<strong>Sende Audio...</strong><div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>';
+      audioMessage.appendChild(audioSpinner);
+      console.log("Sending the blob data:", data);
+      console.log("Downloadable audio", window.URL.createObjectURL(data));
+      axios({
+        method: "post",
+        url: "http://localhost:3000/audio",
+        headers: {
+          "Content-Type": "audio/webm",
+          //   "Content-Type": "multipart/form-data",
         },
+        data: formData,
+      })
+        .then(function (response) {
+          console.log(response);
+          // vm.audioMsg = true;
+          audioMessage.innerHTML =
+            '<strong><p style="color:green">Audio versendet!</p></strong>';
+        })
+        .catch(function (error) {
+          console.log(error);
+          // vm.audioMsg = false;
+          audioMessage.innerHTML =
+            '<strong><p style="color:red">Audio nicht versendet!</p></strong>';
+        });
     },
+  },
 };
 </script>
